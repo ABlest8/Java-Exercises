f0b918fdd5b4ff6c41ed93dc14977e9121a81dfe
ABlest8 <ablest0717@gmail.com>
2022-03-16 13:25:52 +0100

    exercises

---
+++ exercise 2/Even-or-Odd.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int randomNumber = (int) (Math.random() * (100 - 1));
+        if (randomNumber%2 == 0)
+        {
+            System.out.println(randomNumber + " is an even number");
+        }
+        else
+        {
+            System.out.println(randomNumber + " is an odd number");
+        }
+
+    }
+}
